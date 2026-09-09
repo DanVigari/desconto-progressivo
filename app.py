3b4ad5b117e35b7d74545ddc429b17683e25c7a5
Danilo <320537480+DanVigari@users.noreply.github.com>
2026-09-09 08:35:20 -0300

    fix: corrigido saída de valor total da compra quando era 5% de desconto.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ valor_compra = float(input("Digite o valor da compra: ")) #entrada de valor da c
 if valor_compra < 200: #condição para aplicar desconto de 5%
     desconto = valor_compra * 0.05 #calculo do desconto
     valor_total = valor_compra - desconto #subtrai o desconto do valor total
-    print(f"Desconto de 5% aplicado. Valor do desconto: R${desconto:.2f}") #saída do valor do desconto
+    print(f"Desconto de 5% aplicado. Valor do desconto: R${desconto:.2f}. Valor total da compra: R${valor_total:.2f}") #saída do valor do desconto
 elif valor_compra >= 200 and valor_compra < 300: #condição para aplicar desconto de 10%
     desconto = valor_compra * 0.10 #calculo do desconto
     valor_total = valor_compra - desconto #subtrai o desconto do valor total
